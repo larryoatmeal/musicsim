5421889fae9bbf3973b7a3d65261ff507bf71b74
Larry Wang <larryoatmeal@github.com>
2019-01-10 17:35:34 -0500

    reference separated, and generating video

---
+++ CPP/cpu_only/src/SimState.h
@@ -0,0 +1,198 @@
+#ifndef SIM_STATE_H
+#define SIM_STATE_H
+
+
+#include <cmath>
+#include <algorithm> 
+#include "sim_constants.h"
+
+
+float* alloc_grid(){
+  return (float *)calloc(N_TOTAL, sizeof(float));
+}
+
+int index_of_padded(int w, int h){
+  return (w + PAD_HALF) + (h + PAD_HALF) * (STRIDE_Y);
+}
+
+class SimState{
+    public:
+        SimState(){
+            walls = alloc_grid();
+            excitor = alloc_grid();
+            beta = alloc_grid();
+            sigma = alloc_grid();
+            aux_data = (int *)calloc(N_TOTAL, sizeof(int));
+
+            init();
+
+            p = alloc_grid();
+            v_x = alloc_grid();
+            v_y = alloc_grid();
+
+            p_prev = alloc_grid();
+            v_x_prev = alloc_grid();
+            v_y_prev = alloc_grid();
+        }
+
+
+        void step(){
+            for(int x = 0; x < W; x++){
+                for(int y = 0; y < H; y++){
+                    AudioKernel(
+                        x,
+                        y
+                    );
+                }
+            }
+            std::swap<float *>(p_prev, p);
+            std::swap<float *>(v_x_prev, v_x);
+            std::swap<float *>(v_y_prev, v_y);
+        }
+
+        ~SimState(){
+            free(walls);
+            free(excitor);
+            free(beta);
+            free(sigma);
+            free(aux_data);
+            free(p);
+            free(v_x);
+            free(v_y);
+            free(p_prev);
+            free(v_x_prev);
+            free(v_y_prev);
+        }
+
+    int GetWidth(){
+        return W;
+    }
+    int GetHeight(){
+        return H;
+    }
+    float GetPressure(int x, int y){
+        return p[index_of_padded(x, y)];
+    }
+
+    private:
+        float *walls = 0;
+        float *excitor = 0;
+        float *beta = 0;
+        float *sigma = 0;
+        int *aux_data = 0;
+        float *p = 0;
+        float *v_x = 0;
+        float *v_y = 0;
+        float *p_prev = 0;
+        float *v_x_prev = 0;
+        float *v_y_prev = 0;
+
+
+    void init(){
+    //walls
+        for(int i = 40; i < 150; i++){
+        walls[index_of_padded(i, 50)] = 1; //
+        walls[index_of_padded(i, 55)] = 1;
+        }
+
+        walls[index_of_padded(55, 130)] = 0;
+        walls[index_of_padded(55, 100)] = 0;
+
+        //excitor
+        for(int i = 51; i < 55; i++){
+        excitor[index_of_padded(40, i)] = 1;
+        }
+
+        //beta
+        for(int i = 0; i < N_TOTAL; i++){
+        beta[i] = 1 - (walls[i] + excitor[i]);
+        }
+
+        //sigma
+        for(int i = 0; i < PML_LAYERS + 1; i++){
+        for(int x = i; x < W - i; x++){
+            for(int y = i; y < H - i; y++){
+            sigma[index_of_padded(x, y)] = 0.5 / DT * (PML_LAYERS - i)/PML_LAYERS;
+            }
+        }
+        }
+
+        for(int i = 0; i < N_TOTAL; i++){
+        aux_data[i] = ( (int) walls[i] << 2) + ( (int) excitor[i] << 1) + (int) beta[i];
+        }
+    }
+
+    int getBeta(
+        int *aux,
+        int i
+    ){
+        return std::min(aux[i] & (1 << 0), 1);
+    }
+
+    int getExcitor(
+        int *aux,
+        int i
+    ){
+        return std::min(aux[i] & (1 << 1), 1);
+    }
+
+    int getWall(
+        int *aux,
+        int i
+    ){
+        return std::min(aux[i] & (1 << 2), 1);
+    }
+
+    float pressureStep(
+        int i
+    )
+    {
+        float divergence = v_x_prev[i] - v_x_prev[i - STRIDE_X] + v_y_prev[i] - v_y_prev[i - STRIDE_Y];
+        float p_denom = 1 + (1 - getBeta(aux_data, i) + sigma[i]) * DT;
+        return (p_prev[i] - COEFF_DIVERGENCE * divergence)/p_denom;
+    }
+
+
+    void AudioKernel(
+        int idx,
+        int idy
+        )
+        {
+        // int idx=blockIdx.x*blockDim.x+threadIdx.x;
+        // int idy=blockIdx.y*blockDim.y+threadIdx.y;
+        int i = (idx + PAD_HALF) + STRIDE_Y * (idy + PAD_HALF);
+
+        //PRESSURE------------------------------
+
+        float p_current = pressureStep(i);
+        float p_right = pressureStep(i + STRIDE_X);
+        float p_down = pressureStep(i + STRIDE_Y);
+        p[i] = p_current;
+
+        //VB------------------------------
+        //TODO: not sure if this is supposed to be previous or next pressure
+        float delta_p = std::max(P_MOUTH - p_prev[p_bore_index], 0.0f);
+        float vb_x = 0;
+        float vb_y = 0;
+        int wall = getWall(aux_data, i);
+        int excitor = getExcitor(aux_data, i);
+        int wall_down = getWall(aux_data, i + STRIDE_Y);
+        vb_x = excitor * (1 - delta_p / DELTA_P_MAX) * sqrt(2 * delta_p / RHO) * VB_COEFF / num_excite;
+        vb_y = wall * ADMITTANCE * p_current + wall_down * -ADMITTANCE * p_down;
+
+        //VELOCITY------------------------------
+        int beta_current = getBeta(aux_data, i);
+
+        float beta_x = std::min(beta_current, getBeta(aux_data, i + STRIDE_X));
+        float grad_x = p_right - p_current;
+        float sigma_prime_dt_x = (1 - beta_x + sigma[i]) * DT;
+        v_x[i] = beta_x * v_x_prev[i] - beta_x * beta_x * COEFF_GRADIENT * grad_x + sigma_prime_dt_x * vb_x;
+
+        float beta_y = std::min(beta_current, getBeta(aux_data, i + STRIDE_Y));
+        float grad_y = p_down - p[i];
+        float sigma_prime_dt_y = (1 - beta_y + sigma[i]) * DT;
+        v_y[i] = beta_y * v_y_prev[i] - beta_y * beta_y * COEFF_GRADIENT * grad_y + sigma_prime_dt_y * vb_y;
+    }
+
+};
+#endif // SIM_STATE_H
--- CPP/cpu_only/src/main.cpp
+++ CPP/cpu_only/src/main.cpp
@@ -7,223 +7,73 @@
 #include <string>
 #include <math.h>
 #include <fstream>
-using namespace std;
+#include <sstream>      // std::stringstream
+#include <iomanip>
+#include <stdlib.h>
 
-typedef float number;
-
-number RHO = 1.1760;
-number C = 3.4723e2;
-number GAMMA = 1.4017;
-number MU = 1.8460e-5;
-number PRANDLT = 0.7073;
-number DS = 3.83e-3;
-number DT = DS / (sqrt(2) * C) * 0.999999;  // make sure we're actually below the condition;
-number DT_LISTEN = 1.0 / 44000;
-int SAMPLE_EVERY_N = (int) round(DT_LISTEN / DT);
-number REAL_LISTENING_FREQUENCY = 1 / (SAMPLE_EVERY_N * DT);
-number AN = 0.01;
-number ADMITTANCE = 1 / (RHO * C * (1 + sqrt(1 - AN)) / (1 - sqrt(1 - AN)));
-
-number COEFF_DIVERGENCE = RHO * C * C * DT/DS;
-number COEFF_GRADIENT = DT/RHO/DS;
-
-number H_BORE = 0.015;  // 15mm, bore diameter of clarinet
-number H_DS = H_BORE * DS;
-number W_J = 1.2e-2;
-number H_R = 6e-4;
-number K_R = 8e6;
-number DELTA_P_MAX = K_R * H_R;
-number W_J_H_R = W_J * H_R;
-number VB_COEFF = W_J_H_R / H_DS;
-
-int PML_LAYERS = 6;
-
-int W = 220;
-int H = 110;
-int STRIDE_X = 1;
-int STRIDE_Y = W;
-
-
-
-
-float P_MOUTH = 3000;
-
-int p_bore_index = 0;
-int num_excite = 3;
-int listen_index = 0;
-
-
-int SIMULATION_SIZE = W * H;
-int PADDING_HALF = STRIDE_Y + STRIDE_X;
-int PADDING = PADDING_HALF * 2;
-// int PADDED_SIZE = (W + 2) * (H + 2);
-// int PAD_ONE_END = (PADDED_SIZE - SIMULATION_SIZE) / 2;
-
-
-int index_of(int w, int h){
-  return w + h * STRIDE_Y;
-}
-
-int index_of_padded(int w, int h){
-  return index_of(w, h) + PADDING_HALF;
-}
-
-int N = 10000;
-int NUM_AUDIO_SAMPLES = N/SAMPLE_EVERY_N;
-
-number* alloc_grid(){
-  return (number *)calloc(SIMULATION_SIZE + PADDING, sizeof(number));
-}
-
-void init(number *walls, number *excitor, number *beta, number *sigma){
-    //walls
-
-    for(int i = 40; i < 150; i++){
-      walls[index_of_padded(i, 50)] = 1; //
-      walls[index_of_padded(i, 55)] = 1;
-    }
-
-    walls[index_of_padded(55, 130)] = 0;
-    walls[index_of_padded(55, 100)] = 0;
-
-    //excitor
-    for(int i = 51; i < 55; i++){
-      excitor[index_of_padded(40, i)] = 1;
-    }
-
-    p_bore_index = index_of_padded(41, 53);
-    listen_index = index_of_padded(45, 155);
-
-
-    //beta
-    for(int i = 0; i < SIMULATION_SIZE + PADDING; i++){
-      beta[i] = 1 - (walls[i] + excitor[i]);
-    }
-
-    //sigma
-    for(int i = 0; i < PML_LAYERS + 1; i++){
-      for(int x = i; x < W - i; x++){
-        for(int y = i; y < H - i; y++){
-          sigma[index_of_padded(x, y)] = 0.5 / DT * (PML_LAYERS - i)/PML_LAYERS;
-        }
-      }
-    }
-
-}
-
-void swapPtr(number** p1, number **p2)
-{
-  number *temp = *p1;
-  *p1 = *p2;
-  *p2 = temp;
-}
+#include "SimState.h"
 
 int main() {
-  cout << "Hello world!" << endl;
+  std::cout << "Hello world!" << std::endl;
+  
+  SimState sim;
 
-  number *walls = alloc_grid();
-  number *excitor = alloc_grid();
-  number *beta = alloc_grid();
-  number *sigma = alloc_grid();
+  int N = 200000;
+  int SKIP = 10;
 
-  init(walls, excitor, beta, sigma);
+  std::vector<std::string> images;
 
+  for(int i = 0; i < N; i++){
+    sim.step();
+    if(i % SKIP == 0){
+        std::cout << (float) i/N << std::endl;
+        std::stringstream formattedNumber;
 
-  number *p = alloc_grid();
-  number *v_x = alloc_grid();
-  number *v_y = alloc_grid();
+        formattedNumber << std::setfill('0') << std::setw(6) << i/SKIP;
 
-  number *p_prev = alloc_grid();
-  number *v_x_prev = alloc_grid();
-  number *v_y_prev = alloc_grid();
-
-
-  for(int n = 0; n < N; n++){
-    //note the way this works with the padding:
-    //for x derivatives, will WRAP to next line
-    //probably okay since in PML layer anyway
-
-    number max_p = 0;
-    int max_i = 0;
-    for(int i = PADDING_HALF; i < SIMULATION_SIZE + PADDING_HALF; i++){
-      // gradient
-      number divergence = v_x_prev[i] - v_x_prev[i - STRIDE_X] + v_y_prev[i] - v_y_prev[i - STRIDE_Y];
-      number p_denom = 1 + (1 - beta[i] + sigma[i]) * DT;
-      p[i] = (p_prev[i] - COEFF_DIVERGENCE * divergence)/p_denom;
-
-      if(p[i] > max_p){
-        max_p = p[i];
-        max_i = i;
-      }
-    }
-
-    number delta_p = P_MOUTH - p[p_bore_index];
-    for(int i = PADDING_HALF; i < SIMULATION_SIZE + PADDING_HALF; i++){
-
-      number vb_x = 0;
-      number vb_y = 0;
-
-      //check if wall
-      if(excitor[i] && delta_p > 0){
-        vb_x = (1 - delta_p / DELTA_P_MAX) * sqrt(2 * delta_p / RHO) * VB_COEFF / num_excite;
-      }
-      else if(walls[i]){
-        vb_y = ADMITTANCE * p[i];
-      }
-      else if(walls[i + STRIDE_Y]){
-        vb_y = -ADMITTANCE * p[i + STRIDE_Y];
-      }
-
-      number beta_x = min(beta[i], beta[i + STRIDE_X]);
-      number grad_x = p[i + STRIDE_X] - p[i];
-      number sigma_prime_dt_x = (1 - beta_x + sigma[i]) * DT;
-      v_x[i] = beta_x * v_x_prev[i] - beta_x * beta_x * COEFF_GRADIENT * grad_x + sigma_prime_dt_x * vb_x;
-
-      number beta_y = min(beta[i], beta[i + STRIDE_Y]);
-      number grad_y = p[i + STRIDE_Y] - p[i];
-      number sigma_prime_dt_y = (1 - beta_y + sigma[i]) * DT;
-      v_y[i] = beta_y * v_y_prev[i] - beta_y * beta_y * COEFF_GRADIENT * grad_y + sigma_prime_dt_y * vb_y;
-    }
-
-    if(n % 100 == 0){
-      std::string s = "out" + std::to_string(n) + ".png";
-
-      Image image(W, H);
-      for(int y = 0; y < H; y++){
-        for(int x = 0; x < W; x++){
-          // int i = index_of_padded(x, y);
-          // number p_denom = (1 - beta[i] + sigma[i]);
-          // number val = p_denom;
-          number val = max(min(p[index_of_padded(x, y)] * 1000, 1.0f), -1.0f);
-
-          if(val < 0){
-            Vector3f color(0, -val, 0);
-            image.setPixel(x, y, color);
-          }else{
-            Vector3f color(val, 0, 0);
-            image.setPixel(x, y, color);
-
-          }
+        std::string s = "img" + formattedNumber.str() + ".png";
+        Image image(sim.GetWidth(), sim.GetHeight());
+        for(int x = 0; x < sim.GetWidth(); x++){
+            for(int y = 0; y < sim.GetHeight(); y++){
+                float p = sim.GetPressure(x, y);
+                float col = std::max(std::min(p * 1000, 1.0f), -1.0f);
+                if(col < 0){
+                    Vector3f color(0, -col, 0);
+                    image.setPixel(x, y, color);
+                }else{
+                    Vector3f color(col, 0, 0);
+                    image.setPixel(x, y, color);
+                }
+            }      
         }
-      }
-      image.savePNG(s);
+        images.push_back(s);
+
+        image.savePNG(s);
+
+
+       
+        // std::string video_cmd = "ffmpeg -framerate 24 -i img%03d.png output.mp4";
+
+
     }
 
+    
 
-    swapPtr(&p, &p_prev);
-    swapPtr(&v_x, &v_x_prev);
-    swapPtr(&v_y, &v_y_prev);
   }
 
-  free(beta);
-  free(sigma);
-  free(p);
-  free(v_x);
-  free(v_y);
-  free(p_prev);
-  free(v_x_prev);
-  free(v_y_prev);
+    std::stringstream video_cmd;
+    int framerate = 24;
+    video_cmd << "ffmpeg" << " " << "-framerate" << " " << 24 << " -i img%06d.png -pix_fmt yuv420p output.mp4";
+    std::string video = video_cmd.str();
+    std::cout << video << std::endl;
 
+    system(video_cmd.str().c_str());
+
+    for(int i = 0; i < images.size(); i++){
+        std::string s = images[i];
+        std::string rm = "rm " + s;
+        system(rm.c_str());
+    }
 
   return 0;
 }
