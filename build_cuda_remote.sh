87185079995ce900255af8d6ac06e1deb6cb318c
Larry Wang <larryoatmeal@github.com>
2019-01-18 15:38:02 -0500

    Scripts for file watching

---
+++ build_cuda_remote.sh
@@ -0,0 +1,2 @@
+#sleep so files can be udpated in time
+ssh larryw@35.226.198.237 'sleep 1; make -C ~/code/CPP/gpu/sim/ clean; make -C ~/code/CPP/gpu/sim/ libSim.a; make -C ~/code/CPP/cpu_only/build;'
